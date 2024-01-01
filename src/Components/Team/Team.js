92af4b33601597822f274f1749bfba6bc9f04682
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2024-01-01 16:47:04 +0600

    Feat: All Frontend Part done except API binding

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -1,26 +1,59 @@
 import React from "react";
 import { useState } from "react";
 
+const getYear = () => {
+  const d = new Date();
+  const y = d.getFullYear();
+  return y;
+};
+
+const Images = [
+  "./images/image1.jpg",
+  "./images/image2.jpg",
+  "./images/image3.jpg",
+  "./images/image4.jpg",
+];
+
 const Team = ({ team = [] }) => {
-  const [teamImgLink, setTeamImgLink] = useState();
-  const [teamPagination, setTeamPagination] = useState([]);
+  const [year, setYear] = useState(getYear);
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const updateYear = (operator) => {
+    if (operator === "+") {
+      setYear(year + 1);
+      setImageIndex(imageIndex + 1);
+    } else {
+      setYear(year - 1);
+      setImageIndex(imageIndex - 1);
+    }
+  };
+
   return (
     <div className="mt-5 mb-10 p-5">
       <div className="w-11/12 lg:w-4/5 mx-auto rounded-md">
         <img
-          src="https://iili.io/JdaHhFV.jpg"
+          src={Images[imageIndex]}
           alt=""
           className="w-full rounded-md shadow-sm"
         />
       </div>
       <div className="w-3/5 lg:w-2/5 mt-5 mx-auto flex justify-around items-center text-[#939393]">
-        <button className="text-xl lg:text-2xl 2xl:text-2xl hover:text-orange-500">
+        <button
+          className="text-xl lg:text-2xl 2xl:text-2xl hover:text-orange-500"
+          onClick={() => updateYear("-")}
+          disabled={imageIndex === 0}
+        >
           {"<"}
         </button>
         <button className="text-lg lg:text-xl 2xl:text-2xl hover:text-orange-500 uppercase underline decoration-orange-500 underline-offset-2">
-          <span className="hidden lg:inline">Team </span>2023
+          <span className="hidden lg:inline">Team </span>
+          {year}
         </button>
-        <button className="text-xl lg:text-2xl 2xl:text-2xl hover:text-orange-500">
+        <button
+          className="text-xl lg:text-2xl 2xl:text-2xl hover:text-orange-500"
+          onClick={() => updateYear("+")}
+          disabled={imageIndex === Images.length - 1}
+        >
           {">"}
         </button>
       </div>
