0ff3d46fdefddc08eab24d391ae607785adb3b37
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2024-01-13 19:26:58 +0600

    Feat/team members (#4)
    * Feat: All Frontend Part done except API binding
    
    * Functionality Added

--- src/Components/Advisors/Advisors.js
+++ src/Components/Advisors/Advisors.js
@@ -1,26 +1,60 @@
 import React, { useState } from "react";
 
+const getYear = () => {
+  const d = new Date();
+  const y = d.getFullYear();
+  return y;
+};
+
+const Images = [
+  "./images/image1.jpg",
+  "./images/image2.jpg",
+  "./images/image3.jpg",
+  "./images/image4.jpg",
+];
+
 const Advisors = ({ advisors = [] }) => {
-  const [teachersImgLink, setTeachersImgLink] = useState();
-  const [teachersPagination, setTeachersPagination] = useState([]);
+  const [year, setYear] = useState(getYear);
+  const [imageIndex, setImageIndex] = useState(Images.length - 1);
+
+  const updateYear = (operator) => {
+    if (operator === "+") {
+      setYear(year + 1);
+      setImageIndex(imageIndex + 1);
+    } else {
+      setYear(year - 1);
+      setImageIndex(imageIndex - 1);
+    }
+  };
+
   return (
     <div className="mt-5 mb-10 p-5">
       <div className="w-11/12 lg:w-4/5 mx-auto rounded-md">
         <img
-          src="https://iili.io/JdYmzqN.jpg"
+          src={Images[imageIndex]}
           alt=""
           className="w-full rounded-md shadow-sm"
         />
       </div>
       <div className="w-3/5 lg:w-2/5 mt-5 mx-auto flex justify-around items-center text-[#939393]">
-        <button className="text-xl lg:text-2xl 2xl:text-2xl hover:text-orange-500">
-          {"<"}
+        <button
+          className={`text-xl lg:text-2xl 2xl:text-2xl hover:text-orange-500 cursor-pointer ${
+            imageIndex === 0 ? "hidden" : null
+          }`}
+          onClick={() => updateYear("-")}
+        >
+          {`<${year - 1}`}
         </button>
-        <button className="text-lg lg:text-xl 2xl:text-2xl hover:text-orange-500 uppercase underline decoration-orange-500 underline-offset-2">
-          2023
+        <button className="text-lg lg:text-xl 2xl:text-4xl hover:text-orange-500 uppercase underline decoration-orange-500 underline-offset-2">
+          {year}
         </button>
-        <button className="text-xl lg:text-2xl 2xl:text-2xl hover:text-orange-500">
-          {">"}
+        <button
+          className={`text-xl lg:text-2xl 2xl:text-2xl hover:text-orange-500 ${
+            getYear() === year ? "hidden" : null
+          }`}
+          onClick={() => updateYear("+")}
+        >
+          {`${year + 1}>`}
         </button>
       </div>
     </div>
