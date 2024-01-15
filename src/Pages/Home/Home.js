dd3cce70bc58bd073a7d7cbe64a0b940d22c7a47
Hasibul Mahi <hasibulmahi@gmail.com>
2024-01-16 02:26:02 +0600

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -134,7 +134,7 @@ const Home = () => {
                   {/* <button className="bg-[#EA9723] py-1 xl:py-2 px-2 xl:px-4 text-xs xl:text-2xl text-white font-semibold">
                     Read More
                   </button> */}
-                  <button className="text-l w-32 h-12 bg-[#EA9723] text-white relative overflow-hidden group z-10 hover:text-white duration-1000">
+                  <button className="py-1 xl:py-2 px-2 xl:px-4 text-xs xl:text-2xl text-white font-semibold bg-[#EA9723] text-white relative overflow-hidden group z-10 hover:text-white duration-1000">
                     <span className="absolute bg-orange-400 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
                     <span className="absolute bg-orange-300 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
                     Read More
