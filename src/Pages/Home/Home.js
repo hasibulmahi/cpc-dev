c4b5d7c033b860196cd8c9d6493dfb8755acdc73
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2024-01-19 22:36:53 +0600

    Hero Section Changed (#10)

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -36,6 +36,45 @@ const Home = () => {
   ];
   const [backgroundImage, setBackgroundImage] = useState(bgImages[0]);
   const [firstLoad, setFirstLoad] = useState(false);
+
+  const [currentSlider, setCurrentSlider] = useState(0);
+  const sliders = [
+    {
+      img: "https://source.unsplash.com/1200x640/?nature",
+      title: "Escape 1",
+      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+    },
+    {
+      img: "https://source.unsplash.com/1200x640/?hill",
+      title: "Escape 2",
+      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+    },
+    {
+      img: "https://source.unsplash.com/1200x640/?mountain",
+      title: "Escape 3",
+      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+    },
+    {
+      img: "https://source.unsplash.com/1200x640/?river",
+      title: "Escape 4",
+      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+    },
+    {
+      img: "https://source.unsplash.com/1200x640/?sea",
+      title: "Escape 5",
+      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
+    },
+  ];
+  const prevSlider = () =>
+    setCurrentSlider((currentSlider) =>
+      currentSlider === 0 ? sliders.length - 1 : currentSlider - 1
+    );
+  const nextSlider = () =>
+    setCurrentSlider((currentSlider) =>
+      currentSlider === sliders.length - 1 ? 0 : currentSlider + 1
+    );
+  const isSmallScreen = window.innerWidth <= 768;
+
   useEffect(() => {
     if (
       !localStorage.getItem("first-load") ||
@@ -114,52 +153,132 @@ const Home = () => {
         </div>
 
         {/* Top Banner */}
-        <div className="flex flex-col-reverse md:flex-row gap-x-5 items-center p-5 xl:py-[100px] bg-white">
-          <div className="top-0 -bottom-px 2xl:bottom-0 right-0 left-0 cover-gradient">
-            <div className="flex text-center md:text-left my-3 items-center h-full">
-              <div className="w-full h-max">
-                <div className="text-xs sm:text-sm xl:text-3xl text-[#015694]">
-                  Daffodil International University
-                  {/* Daffodil International{" "}
-                  <Typewriter loop cursor words={["University"]} /> */}
-                </div>
-                <div className="font-bold text-xl xl:text-4xl text-[#015694] mt-0 xl:mt-2">
-                  {/* Computer & <br className="hidden xl:block" /> Programming Club */}
-                  <Typewriter
-                    loop
-                    cursor
-                    words={["Computer & Programming Club"]}
-                  />
-                </div>
-                <div className="text-sm xl:text-xl font-light text-[#646464] mt-0 xl:mt-7">
-                  DIU CPC is the most primitive and extensive club as well as
-                  the biggest club in Daffodil International University. We work
-                  together to explore every field of Computer Science
-                </div>
-                <div className="mt-2 xl:mt-7">
-                  {/* <button className="bg-[#EA9723] py-1 xl:py-2 px-2 xl:px-4 text-xs xl:text-2xl text-white font-semibold">
-                    Read More
-                  </button> */}
-                  <button className="py-1 xl:py-2 px-2 xl:px-4 text-xs xl:text-2xl font-semibold bg-[#EA9723] text-white relative overflow-hidden group z-10 hover:text-white duration-1000">
-                    <span className="absolute bg-orange-400 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
-                    <span className="absolute bg-orange-300 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
-                    Read More
-                  </button>
-                  <button className="ml-2 ring-2 ring-inset ring-[#EA9723] py-1 xl:py-2 px-2 xl:px-4 text-xs xl:text-2xl text-[#656565] font-normal rounded-br-xl xl:rounded-br-[35px] hover:text-orange-400">
-                    Contact Us
-                  </button>
-                </div>
+        <div
+          className="w-full h-72 sm:h-96 md:h-[540px] lg:h-[640px] xl:h-[780px] flex flex-col xl:flex-row items-center justify-center gap-5 lg:gap-10 relative bg-cover before:absolute before:bg-black/50 before:inset-0 transform duration-1000 ease-linear z-50 overflow-hidden"
+          style={{
+            backgroundImage: `url(${
+              currentSlider === 0
+                ? sliders[sliders.length - 1].img
+                : sliders[currentSlider - 1].img
+            })`,
+          }}
+        >
+          {/* arrow */}
+          <div className="absolute bottom-1/4 flex gap-3 z-50 px-5">
+            {/* arrow left */}
+            <button
+              onClick={prevSlider}
+              className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8"
+            >
+              <svg
+                viewBox="0 0 1024 1024"
+                className="w-4 h-4 md:w-6 md:h-6 icon"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#000000"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    fill="#0095FF"
+                    d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
+                  ></path>
+                </g>
+              </svg>
+            </button>
+            {/* arrow right */}
+            <button
+              onClick={nextSlider}
+              className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8"
+            >
+              <svg
+                viewBox="0 0 1024 1024"
+                className="w-4 h-4 md:w-6 md:h-6 icon"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#000000"
+                transform="rotate(180)"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    fill="#0095FF"
+                    d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
+                  ></path>
+                </g>
+              </svg>
+            </button>
+          </div>
+          {/* text container here */}
+          <div className="w-1/2 px-4 left-0 absolute drop-shadow-lg text-white rounded-lg">
+            <div className="w-full h-max">
+              <div className="text-xs sm:text-sm xl:text-3xl text-white">
+                Daffodil International University
+              </div>
+              <div className="font-bold text-xl xl:text-4xl text-[#015694] mt-0 xl:mt-2">
+                <Typewriter
+                  loop
+                  cursor
+                  words={["Computer & Programming Club"]}
+                />
+              </div>
+              <div className="text-sm xl:text-xl font-light text-[#ccc] mt-0 xl:mt-7">
+                DIU CPC is the most primitive and extensive club as well as the
+                biggest club in Daffodil International University. We work
+                together to explore every field of Computer Science
+              </div>
+              <div className="mt-2 xl:mt-7">
+                <button className="text-xl w-32 h-16 bg-sky-500 text-white relative overflow-hidden group z-10 hover:text-white duration-1000 rounded-lg">
+                  <span className="absolute bg-sky-600 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
+                  <span className="absolute bg-sky-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
+                  Read More
+                </button>
+                <button
+                  style={{ borderBottomRightRadius: 60 }}
+                  className="text-xl w-36 h-16 border-2 border-sky-600 text-sky-600 relative overflow-hidden group z-10 hover:text-white duration-1000 rounded-lg ml-3"
+                >
+                  <span className="absolute bg-sky-600 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
+                  <span className="absolute bg-sky-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
+                  Contact Us
+                </button>
               </div>
             </div>
           </div>
-          <div className="w-full">
-            <img
-              src={backgroundImage}
-              alt="Cover"
-              className="xl:h-[400px] 2xl:max-w-[1000px] w-full max-w-[600px] mx-auto"
-            />
+          {/* slider container */}
+          <div className="w-1/2 ml-auto overflow-hidden  absolute -right-5 lg:-right-16 z-50 px-4 py-10">
+            <div
+              className="ease-linear duration-300 flex gap-4 items-center"
+              style={{
+                transform: `translateX(-${
+                  currentSlider * (isSmallScreen ? 98 : 200)
+                }px)`,
+              }}
+            >
+              {/* sliders */}
+              {sliders.map((slide, inx) => (
+                <img
+                  key={inx}
+                  src={slide.img}
+                  className={`h-[180px] sm:h-[200px] lg:h-[320px] min-w-[90px] lg:min-w-[184px] ${
+                    currentSlider - 1 === inx
+                      ? "scale-0"
+                      : "scale-100 delay-500"
+                  } drop-shadow-lg shadow-lg shadow-black bg-black/50 duration-300 rounded-lg z-50`}
+                  alt={slide.title}
+                />
+              ))}
+            </div>
           </div>
         </div>
+
         <div className="bg-[#015694] px-5 xl:px-10 py-0.5 xl:py-5">
           <div className="xl:w-4/5 mx-auto grid grid-cols-5 xl:grid-cols-8 items-center">
             <div className="text-white text-sm xl:text-2xl 2xl:text-3xl font-light">
