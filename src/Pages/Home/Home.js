143c58dab89b008da849ef6bf0febbed23a66a40
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2024-01-23 02:25:15 +0600

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -36,43 +36,15 @@ const Home = () => {
   ];
   const [backgroundImage, setBackgroundImage] = useState(bgImages[0]);
   const [firstLoad, setFirstLoad] = useState(false);
-
   const [currentSlider, setCurrentSlider] = useState(0);
   const sliders = [
-    {
-      img: "https://source.unsplash.com/1200x640/?nature",
-      title: "Escape 1",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
-    {
-      img: "https://source.unsplash.com/1200x640/?hill",
-      title: "Escape 2",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
-    {
-      img: "https://source.unsplash.com/1200x640/?mountain",
-      title: "Escape 3",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
-    {
-      img: "https://source.unsplash.com/1200x640/?river",
-      title: "Escape 4",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
-    {
-      img: "https://source.unsplash.com/1200x640/?sea",
-      title: "Escape 5",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
+    "/images/image1.jpg",
+    "/images/image2.jpg",
+    "/images/image3.jpg",
+    "/images/image4.jpg",
+    "/images/image5.jpg",
   ];
-  const prevSlider = () =>
-    setCurrentSlider((currentSlider) =>
-      currentSlider === 0 ? sliders.length - 1 : currentSlider - 1
-    );
-  const nextSlider = () =>
-    setCurrentSlider((currentSlider) =>
-      currentSlider === sliders.length - 1 ? 0 : currentSlider + 1
-    );
+
   const isSmallScreen = window.innerWidth <= 768;
 
   useEffect(() => {
@@ -112,6 +84,18 @@ const Home = () => {
       }
     }
   }, [userFlags, firstLoad]);
+
+  const nextSlider = () =>
+    setCurrentSlider((currentSlider) =>
+      currentSlider === sliders.length - 1 ? 0 : currentSlider + 1
+    );
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextSlider();
+    }, 3000);
+    return () => clearInterval(intervalId);
+  }, [currentSlider]);
+
   return (
     <>
       <div
@@ -152,73 +136,8 @@ const Home = () => {
           </div>
         </div>
 
-        {/* Top Banner */}
-        <div
-          className="w-full h-72 sm:h-96 md:h-[540px] lg:h-screen flex flex-col xl:flex-row items-center justify-center gap-5 lg:gap-10 relative bg-cover before:absolute before:bg-black/50 before:inset-0 transform duration-1000 ease-linear z-50 overflow-hidden"
-          style={{
-            backgroundImage: `url(${
-              currentSlider === 0
-                ? sliders[sliders.length - 1].img
-                : sliders[currentSlider - 1].img
-            })`,
-          }}
-        >
-          {/* arrow */}
-          <div className="absolute bottom-1/4 flex gap-3 z-50 px-5">
-            {/* arrow left */}
-            <button
-              onClick={prevSlider}
-              className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8"
-            >
-              <svg
-                viewBox="0 0 1024 1024"
-                className="w-4 h-4 md:w-6 md:h-6 icon"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#000000"
-              >
-                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></g>
-                <g id="SVGRepo_iconCarrier">
-                  <path
-                    fill="#0095FF"
-                    d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
-                  ></path>
-                </g>
-              </svg>
-            </button>
-            {/* arrow right */}
-            <button
-              onClick={nextSlider}
-              className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8"
-            >
-              <svg
-                viewBox="0 0 1024 1024"
-                className="w-4 h-4 md:w-6 md:h-6 icon"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#000000"
-                transform="rotate(180)"
-              >
-                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></g>
-                <g id="SVGRepo_iconCarrier">
-                  <path
-                    fill="#0095FF"
-                    d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
-                  ></path>
-                </g>
-              </svg>
-            </button>
-          </div>
-          {/* text container here */}
-          <div className="w-1/2 px-4 left-0 absolute drop-shadow-lg text-white rounded-lg">
+        {/* text container here */}
+        {/* <div className="w-1/2 px-4 left-0 absolute drop-shadow-lg text-white rounded-lg">
             <div className="w-full h-max">
               <div className="text-xs sm:text-sm xl:text-3xl text-white">
                 Daffodil International University
@@ -251,35 +170,97 @@ const Home = () => {
                 </button>
               </div>
             </div>
-          </div>
-          {/* slider container */}
-          <div className="w-1/2 ml-auto overflow-hidden  absolute -right-5 lg:-right-16 z-50 px-4 py-10">
+          </div> */}
+        <div className="w-full mx-auto h-[240px] md:h-[470px] lg:h-[500px] pt-[100px] flex flex-col lg:flex-row items-center overflow-hidden gap-5 lg:gap-10">
+          <div className="relative overflow-hidden flex items-center">
+            {/* dots */}
+            <div className="flex h-fit rounded-full z-50 absolute right-0 bottom-1/2 w-fit gap-1 rotate-90">
+              {sliders.map((_, inx) => (
+                <button
+                  key={inx}
+                  onClick={() => setCurrentSlider(inx)}
+                  className={`rounded-full duration-300 bg-white ${
+                    currentSlider === inx ? "w-10" : "w-2"
+                  } h-2`}
+                ></button>
+              ))}
+            </div>
+            <div className="w-1/2 px-4 left-0 absolute drop-shadow-lg text-white rounded-lg">
+              <div className="w-full h-max">
+                <div className="text-xs sm:text-sm xl:text-3xl text-black">
+                  Daffodil International University
+                </div>
+                <div className="font-bold text-xl xl:text-4xl text-[#015694] mt-0 xl:mt-2">
+                  <Typewriter
+                    loop
+                    cursor
+                    words={["Computer & Programming Club"]}
+                  />
+                </div>
+                <div className="text-sm xl:text-xl font-light text-[#666] mt-0 xl:mt-7">
+                  DIU CPC is the most primitive and extensive club as well as
+                  the biggest club in Daffodil International University. We work
+                  together to explore every field of Computer Science
+                </div>
+                <div className="mt-2 xl:mt-7">
+                  <button className="text-xl w-32 h-16 bg-sky-500 text-white relative overflow-hidden group z-10 hover:text-white duration-1000 rounded-lg">
+                    <span className="absolute bg-sky-600 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
+                    <span className="absolute bg-sky-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
+                    Read More
+                  </button>
+                  <button
+                    style={{ borderBottomRightRadius: 30 }}
+                    className="text-xl w-36 h-16 border-2 border-sky-600 text-sky-600 relative overflow-hidden group z-10 hover:text-white duration-1000 rounded-lg ml-3"
+                  >
+                    <span className="absolute bg-sky-600 w-40 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
+                    <span className="absolute bg-sky-800 w-40 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
+                    Contact Us
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* slider container */}
             <div
-              className="ease-linear duration-300 flex gap-4 items-center"
-              style={{
-                transform: `translateX(-${
-                  currentSlider * (isSmallScreen ? 98 : 200)
-                }px)`,
-              }}
+              className="w-screen ease-linear duration-300 flex flex-col h-[340px] md:h-[400px] transform-gpu relative"
+              style={{ transform: `translateY(-${currentSlider * 100}%)` }}
             >
               {/* sliders */}
-              {sliders.map((slide, inx) => (
-                <img
+              {sliders.map((_, inx) => (
+                <div
                   key={inx}
-                  src={slide.img}
-                  className={`h-[180px] sm:h-[200px] lg:h-[320px] min-w-[90px] lg:min-w-[184px] ${
-                    currentSlider - 1 === inx
-                      ? "scale-0"
-                      : "scale-100 delay-500"
-                  } drop-shadow-lg shadow-lg shadow-black bg-black/50 duration-300 rounded-lg z-50`}
-                  alt={slide.title}
-                />
+                  className="w-1/2 ml-auto duration-200 before:content-['Image'] before:bg-black/20 before:-z-10 before:absolute before:text-3xl before:flex before:justify-center before:items-center before:text-black/40 before:inset-0 relative"
+                >
+                  <img
+                    src={_}
+                    className="w-full h-[340px] md:h-[400px] object-fit"
+                    alt={`Slider - ${inx + 1}`}
+                  />
+                </div>
               ))}
             </div>
           </div>
         </div>
+        <div className="w-2/3 flex justify-evenly h-[100px] rounded-3xl mx-auto bg-[#EF7D14] mt-[30px]">
+          <div className="flex flex-col items-center justify-center font-semibold text-white">
+            <p className="text-xl">8000+</p>
+            <p>Current Members</p>
+          </div>
+          <div className="flex flex-col items-center justify-center font-semibold text-white">
+            <p className="text-xl">1000+</p>
+            <p>Alumni</p>
+          </div>
+          <div className="flex flex-col items-center justify-center font-semibold text-white">
+            <p className="text-xl">19</p>
+            <p>Committee Member</p>
+          </div>
+          <div className="flex flex-col items-center justify-center font-semibold text-white">
+            <p className="text-xl">50</p>
+            <p>Executive</p>
+          </div>
+        </div>
+      </div>
 
-        {/* <div className="bg-[#015694] px-5 xl:px-10 py-0.5 xl:py-5">
+      {/* <div className="bg-[#015694] px-5 xl:px-10 py-0.5 xl:py-5">
           <div className="xl:w-4/5 mx-auto grid grid-cols-5 xl:grid-cols-8 items-center">
             <div className="text-white text-sm xl:text-2xl 2xl:text-3xl font-light">
               Flagship |
@@ -293,75 +274,73 @@ const Home = () => {
           </div>
         </div> */}
 
-        <div className="my-10"></div>
-        {unExpiredEvents?.length !== 0 ? (
-          <>
-            {/* Events Carousel */}
-            <div>
-              <div className="flex justify-center">
-                <div className="text-2xl font-semibold py-2 px-32 xl:px-36 home-heading-bg text-center">
-                  Ongoing
-                  <span className="text-slate-400 dark:text-slate-500">/</span>
-                  Upcoming <span className="text-sky-700">Events</span>
-                </div>
-              </div>
-              <div className="px-5 xl:px-0">
-                <EventsCarousel
-                  unExpiredEvents={unExpiredEvents}
-                ></EventsCarousel>
+      {unExpiredEvents?.length !== 0 ? (
+        <>
+          {/* Events Carousel */}
+          <div className="py-10">
+            <div className="flex justify-center">
+              <div className="text-2xl font-semibold py-2 px-32 xl:px-36 home-heading-bg text-center">
+                Ongoing
+                <span className="text-slate-400 dark:text-slate-500">/</span>
+                Upcoming <span className="text-sky-700">Events</span>
               </div>
             </div>
-            <div className="text-slate-400 dark:text-slate-600 text-2xl font-semibold text-center my-10">
-              -- - --
-            </div>
-          </>
-        ) : (
-          ""
-        )}
-        {/* Wings */}
-        <div>
-          <div className="flex justify-center">
-            <div className="text-2xl font-semibold py-2 px-32 xl:px-36 home-heading-bg text-center">
-              Our <span className="text-sky-700">Wings</span>
+            <div className="px-5 xl:px-0">
+              <EventsCarousel
+                unExpiredEvents={unExpiredEvents}
+              ></EventsCarousel>
             </div>
           </div>
-          <Wings></Wings>
-        </div>
-        <div className="text-slate-400 dark:text-slate-600 text-2xl font-semibold text-center my-10">
-          -- - --
-        </div>
-        {/* Advisors */}
-        <div>
-          <div className="flex justify-center">
-            <div className="text-2xl font-semibold py-2 px-32 xl:px-36 home-heading-bg text-center">
-              Meet The <span className="text-sky-700">Advisors</span>
-            </div>
+          <div className="text-slate-400 dark:text-slate-600 text-2xl font-semibold text-center my-10">
+            -- - --
           </div>
-          <div className="text-slate-500 dark:text-slate-200 text-sm text-center mt-2">
-            The advising teachers of DIU Computer & Programming Club
+        </>
+      ) : (
+        ""
+      )}
+      {/* Wings */}
+      <div className="bg-[#F1F5F9] py-10">
+        <div className="flex justify-center">
+          <div className="text-2xl font-semibold py-2 px-32 xl:px-36 home-heading-bg text-center">
+            Our <span className="text-sky-700">Wings</span>
           </div>
-          <Advisors advisors={advisors}></Advisors>
         </div>
-        <div className="text-slate-400 dark:text-slate-600 text-2xl font-semibold text-center my-10">
-          -- - --
-        </div>
-        {/* Team */}
-        <div>
-          <div className="flex justify-center">
-            <div className="text-2xl font-semibold py-2 px-32 xl:px-36 home-heading-bg text-center">
-              Meet The <span className="text-sky-700">Team</span>
-            </div>
+        <Wings></Wings>
+      </div>
+      <div className="text-slate-400 dark:text-slate-600 text-2xl font-semibold text-center my-10">
+        -- - --
+      </div>
+      {/* Advisors */}
+      <div className="bg-[#F1F5F9] py-10">
+        <div className="flex justify-center">
+          <div className="text-2xl font-semibold py-2 px-32 xl:px-36 home-heading-bg text-center">
+            Meet The <span className="text-sky-700">Advisors</span>
           </div>
-          <div className="text-slate-500 dark:text-slate-200 text-sm text-center mt-2">
-            The core members of DIU Computer & Programming Club
-          </div>
-          <Team team={team}></Team>
         </div>
+        <div className="text-slate-500 dark:text-slate-200 text-sm text-center mt-2">
+          The advising teachers of DIU Computer & Programming Club
+        </div>
+        <Advisors advisors={advisors}></Advisors>
+      </div>
+      <div className="text-slate-400 dark:text-slate-600 text-2xl font-semibold text-center my-10">
+        -- - --
+      </div>
+      {/* Team */}
+      <div className="bg-[#F1F5F9] py-10">
+        <div className="flex justify-center">
+          <div className="text-2xl font-semibold py-2 px-32 xl:px-36 home-heading-bg text-center">
+            Meet The <span className="text-sky-700">Team</span>
+          </div>
+        </div>
+        <div className="text-slate-500 dark:text-slate-200 text-sm text-center mt-2">
+          The core members of DIU Computer & Programming Club
+        </div>
+        <Team team={team}></Team>
+      </div>
 
-        {/* Footer */}
-        <div className="mt-10">
-          <Footer></Footer>
-        </div>
+      {/* Footer */}
+      <div className="mt-10">
+        <Footer></Footer>
       </div>
 
       {/* New User Modal */}
