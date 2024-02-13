f2c749e55a3aa338bcdf0c1b69aa3ba6b6aee671
Hasibul Mahi <hasibulmahi@gmail.com>
2024-02-14 00:24:31 +0600

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 export const colors = {
-  textColor: "text-[#EF7D14]",
-  bgColor: "bg-[#EF7D14]",
-  borderColor: "border-[#EF7D14]",
+  textColor: "text-[#E46127]",
+  bgColor: "bg-[#E46127]",
+  borderColor: "border-[#E46127]",
 
   bgDeep: "bg-[#F39C12]",
 
-  ringHover: "ring-[#EF7D14]",
+  ringHover: "ring-[#E46127]",
   ringDeep: "ring-[#F39C12]",
 };
